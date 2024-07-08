7856686e3c04de58752d396106cbcff2534e8c31
janarthanan7205 <112924931+janarthanan7205@users.noreply.github.com>
2024-07-08 22:27:15 +0530

    Update LEAPYEARORNOT.java

--- LEAPYEARORNOT.java
+++ LEAPYEARORNOT.java
@@ -1,17 +1,23 @@
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main (String[]args)
+    {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter A:");
-        int a = scanner.nextInt();
-        if (a % 4 == 0) {
-            System.out.println("It is Leap year!");
-        } else {
-            System.out.println("It is Not A Leap year");
-        }
+        System.out.println("Enter a Year:");
+        int year = scanner.nextInt();
+
+        if (year % 400 == 0)
+            System.out.println (year + " is a Leap Year");
+
+        else if (year % 4 == 0 && year % 100 != 0)
+            System.out.println (year + " is a Leap Year");
+
+        else
+            System.out.println (year + " is not a Leap Year");
+
     }
 }
+
 
 
 

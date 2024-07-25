7cded04cd3a4928d6b87799f659b3eea9f23c2ef
janarthanan7205 <112924931+janarthanan7205@users.noreply.github.com>
2024-07-25 22:59:05 +0530

    Create hello.java

---
+++ PRIMEORNOTwithingivenrange/hello.java
@@ -0,0 +1,22 @@
+package com.jana;
+public class hello {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void printPrimesInRange(int start, int end) {
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}

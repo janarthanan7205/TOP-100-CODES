5ed16d6c68377a540938aa119842dc6e4d5e2a06
janarthanan7205 <112924931+janarthanan7205@users.noreply.github.com>
2024-07-25 22:40:05 +0530

    Add files via upload

---
+++ PRIME OR NOT/hello.java
@@ -0,0 +1,15 @@
+package com.jana;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=scanner.nextInt();
+        hello hello = new hello();
+        hello.helloWorld(n);
+        scanner.close();
+
+    }
+}
